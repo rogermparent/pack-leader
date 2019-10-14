20c217f1847e5af536fed1c3b5a564822950e7a2
rogermparent <rogermparent@gmail.com>
2019-10-14 06:14:26 -0400

    Make a hacky temporary patch to fix functionality temporarily
    Use index as form keys to fix the unfocus problem
    A way to make stable UUID keys is desparately needed

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,11 @@ import Layout from "../components/layout";
 import packageFormReducer from "../reducers/package-form";
 
 import {styled} from 'linaria/react';
+import { useUID, useUIDSeed } from 'react-uid';
 
 const initialState = {
     nestSettings: [
-        { name: "bag", amount: 100 }
+        { name: "Bag", amount: 100 }
     ]
 };
 
@@ -28,7 +29,7 @@ margin: 0.5em 0;
 width: 100%;
 `;
 
-const NestSettingListItem = styled.li`
+const NestSettingListItemStyle = styled.li`
 display: flex;
 align-items: center;
 justify-content: center;
@@ -64,70 +65,118 @@ border-radius: 0.25em;
 padding: 0 0.5em;
 `;
 
+const PackageCalculator = styled.div`
+`;
+
+const CalculatorResults = styled.ul`
+list-style: none;
+margin: 0.5em;
+padding: 0.5em;
+text-align: center;
+display: flex;
+flex-flow: row wrap;
+li {
+  >b {
+    display: block;
+  }
+  margin: 0.25em;
+  padding: 0.25em;
+  min-width: 5em;
+  border: 1px solid gray;
+  border-radius: 0.25em;
+}
+`;
+
+const NestSettingListItem = (({setting, dispatch, index}) => {
+    const uid = index; // TODO figure out a way to make a real stable key
+    return(
+    <NestSettingListItemStyle
+      key={uid}>
+      <input
+        type="text"
+        placeholder="Package Name"
+        value={setting.name}
+        onChange={e=>dispatch({
+            type: "UPDATE_SETTING",
+            index,
+            field: "name",
+            value: e.target.value
+        })}
+      />
+      <input
+        type="number"
+        placeholder="Amount nested"
+        value={setting.amount}
+        onChange={e=>dispatch({
+            type: "UPDATE_SETTING",
+            index,
+            field: "amount",
+            value: e.target.value
+        })}
+      />
+      <button
+        onClick={e=>dispatch({
+            type: "REMOVE_SETTING",
+            index
+        })}
+      >X</button>
+    </NestSettingListItemStyle>
+    );
+});
+
 const IndexPage = () => {
     const [state, dispatch] = useReducer(packageFormReducer, initialState);
+    const uid = useUIDSeed();
 
     return(
         <Layout>
-          <CalculatorForm>
-            <TotalPartInput
-              type="number"
-              value={state.totalParts}
-              onChange={e=>dispatch({
-                  type: "UPDATE_TOTAL",
-                  value: e.target.value
-              })}
-              placeholder="Total Parts"
-            />
-            <NestSettingsList>
+          <PackageCalculator>
+            <CalculatorResults>
               {
-                  state.nestSettings.map((setting, index)=>(
-                      <NestSettingListItem key={`${index}-${setting.name}-${setting.amount}`}>
-                        <input
-                          type="text"
-                          placeholder="Package Name"
-                          value={setting.name}
-                          onChange={e=>dispatch({
-                              type: "UPDATE_SETTING",
-                              index,
-                              field: "name",
-                              value: e.target.value
-                          })}
-                        />
-                        <input
-                          type="number"
-                          placeholder="Amount nested"
-                          value={setting.amount}
-                          onChange={e=>dispatch({
-                              type: "UPDATE_SETTING",
-                              index,
-                              field: "amount",
-                              value: e.target.value
-                          })}
-                        />
-                        <button
-                          onClick={e=>dispatch({
-                              type: "REMOVE_SETTING",
-                              index
-                          })}
-                        >X</button>
-                      </NestSettingListItem>
+                  state.result && state.result.map((packageResult, index)=>(
+                      <li key={uid(packageResult, index)}>
+                        <b>{packageResult.name}</b>
+                        <div>
+                          <span>{packageResult.amount}</span>
+                          <span> @ </span>
+                          <span>{packageResult.partCount}</span>
+                        </div>
+                      </li>
                   ))
               }
-            </NestSettingsList>
+            </CalculatorResults>
+            <CalculatorForm>
+              <TotalPartInput
+                type="number"
+                value={state.totalParts}
+                onChange={e=>dispatch({
+                    type: "UPDATE_TOTAL",
+                    value: e.target.value
+                })}
+                placeholder="Total Parts"
+              />
+              <NestSettingsList>
+                {
+                    state.nestSettings.map((setting, index)=>(
+                        <NestSettingListItem
+                          setting={setting}
+                          dispatch={dispatch}
+                          index={index}
+                        />
+                    ))
+                }
+              </NestSettingsList>
 
-            <ControlButton onClick={e=>dispatch({
-                type: "ADD_SETTING"
-            })}>Add Package Type</ControlButton>
+              <ControlButton onClick={e=>dispatch({
+                  type: "ADD_SETTING"
+              })}>Add Package Type</ControlButton>
 
-            <ControlButton onClick={e=>dispatch({
-                type: "CALCULATE"
-            })}>Calculate</ControlButton>
+              <ControlButton onClick={e=>dispatch({
+                  type: "CALCULATE"
+              })}>Calculate</ControlButton>
 
-          </CalculatorForm>
-          <div>
-            <pre>{JSON.stringify(state.result, undefined, 2)}</pre>
-          </div>
+            </CalculatorForm>
+          </PackageCalculator>
         </Layout>
     );
 };
